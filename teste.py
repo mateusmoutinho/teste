c46e7dde7e9df2576e394a80140828f7611e0998
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-01-23 13:11:57 -0300

    at

--- teste.py
+++ teste.py
@@ -2,8 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Web App with Python Flask!'
-
-app.run(host='0.0.0.0', port=81)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
